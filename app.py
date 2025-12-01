8cee11171d8a58d294ea229d6450e6bac0be7bab
ayyandar09 <ayyandar09@gmail.com>
2025-12-01 18:26:00 +0500

    fixing pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-        return {"message": "Hello from Kubernetes CI/CD!"}
+        return {"message": "Hello from Kubernetes yes CI/CD!"}
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0", port=5000)
